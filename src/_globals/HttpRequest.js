80b4de3b7d4ffe7996696f76fa8bf2cfabd85272
trinadhkoya <trinadh.koya@zaggle.in>
2018-09-22 21:08:02 +0530

    setting up Http Requests with fetch

--- src/_globals/HttpRequest.js
+++ src/_globals/HttpRequest.js
@@ -1,12 +1,18 @@
 import {Platform} from 'react-native';
 import {API} from './API';
+import DeviceInfo from 'react-native-device-info';
 
 function count(obj) {
     return Object.keys(obj).length;
 }
 
 /**
- *
+ * This is to handle GET Requests
+ * @param type -> fo instance 'baseURL'.These might change when you are handling with Multiple Base URLS
+ * @param api_name -> for instance '/user/'
+ * @param parameters
+ * @returns {Promise<T | never>}
+ * @constructor
  */
 export const GET = (type, api_name, parameters) => {
     let str = '';
@@ -14,22 +20,21 @@ export const GET = (type, api_name, parameters) => {
         let theKey = Object.keys(parameters)[i];
         str = `${str}&${theKey}=${parameters[theKey]}`;
     }
-    // str = `${str}&app_version=${settings.APP_VERSION}`;
+    str = `${str}&app_version=${DeviceInfo.getReadableVersion()}`;
     str = `${str}&platform=${(Platform.OS === 'android') ? 'android' : 'iOS'}`;
     str = str.toString().substr(1);
-    let BASE = API.BASE_URL_1;
+    let BASE_URL = API.BASE_URL_1;
     switch (type) {
-    case 'GDD':
-        BASE = API.BASE_URL_1;
+    case 'WEB':
+        BASE_URL = API.BASE_URL_1;
         break;
-    case 'MOB':
-        BASE = API.BASE_URL_2;
+    case 'MOBILE':
+        BASE_URL = API.BASE_URL_2;
         break;
     default:
         break;
     }
-
-    return fetch(BASE + api_name + '?' + str, {method: 'GET'})
+    return fetch(BASE_URL + api_name + '?' + str, {method: 'GET'})
         .then((response) => {
             return response.json();
         })
@@ -40,4 +45,48 @@ export const GET = (type, api_name, parameters) => {
             return error;
         });
 
+};
+
+/**
+ * This is to handle POST Requests
+ * @param type
+ * @param api_name
+ * @param parameters
+ * @returns {Promise<T | never>}
+ * @constructor
+ */
+export const POST = (type, api_name, parameters) => {
+
+    let newParameters = parameters;
+    if (!newParameters.type) {
+        let platform = (Platform.OS === 'android') ? 'android' : 'iOS';
+        newParameters['platform'] = platform;
+        newParameters['app_version'] = DeviceInfo.getReadableVersion();
+    }
+    let BASE_URL = API.BASE_URL_1;
+    switch (type) {
+    case 'WEB':
+        BASE_URL = API.BASE_URL_1;
+        break;
+    case 'MOBILE':
+        BASE_URL = API.BASE_URL_2;
+        break;
+    default:
+        break;
+    }
+    return fetch(BASE_URL + api_name, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newParameters)
+    })
+        .then((response) => response.text())
+        .then((responseText) => {
+            return responseText;
+        })
+        .catch((error) => {
+            return error;
+        });
 };
